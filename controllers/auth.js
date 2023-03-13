844279489efcd28412c0a5040c48173d950ae155
hrom1 <hrom012@gmail.com>
2023-03-13 18:23:57 +0200

    add register function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
-const register = (req, res) => {
-    res.send('register user');
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+
+const register = async (req, res) => {
+    const user = await User.create({...req.body});
+
+    res.status(StatusCodes.CREATED).json({token: user.createJWT()});
 };
 
 const login = (req, res) => {
-    res.send('login user');
+    res.send('login suser');
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -19,8 +21,17 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide password'],
         minLength: 6,
-        maxLength: 20,
     },
 });
 
+userSchema.pre('save', async function (){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.createJWT = function (){
+    return jwt.sign({userId: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME,
+    });;
+}
 module.exports = mongoose.model('User', userSchema);
