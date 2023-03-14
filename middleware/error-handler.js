dff3d1394883fc8515b58aac64f66d22edf76e28
hrom1 <hrom012@gmail.com>
2023-03-14 19:32:10 +0200

    add update and delete job functions

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,16 @@
-const {CustomAPIError} = require("../errors");
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if(err instanceof CustomAPIError){
-        res.status(err.statusCode).json({message: err.message});
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong try again later',
+    };
+    if(err.code===11000){
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
+        customError.statusCode = 400;
     }
-    else{
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: err.message});
-    }
+    // res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: customError.msg});
+    return res.status(customError.statusCode).json({msg: customError.msg});
 }
 
 module.exports = errorHandlerMiddleware;
